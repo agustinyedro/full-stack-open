a608095e227a1f88b47e03fdcaf5bdc0023b96c0
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2024-04-07 23:26:26 -0300

    exercises 2.5

--- Parte2/src/Course.jsx
+++ Parte2/src/Course.jsx
@@ -1,17 +1,19 @@
-import Header from "./Header"
-import Content from "./Content"
-import Total from "./Total"
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
 
-const Course = ({course}) => {
-    return (
-        <div>
-            <Header course={course[0].name} />
-            <Content parts={course[0].parts} />
-            <Total parts={course[0].parts} />
-            <Header course={course[1].name} />
-            <Content parts={course[1].parts} />
-            <Total parts={course[1].parts} />
+const Course = ({ course }) => {
+  return (
+    <div>
+        <h1>Web development curriculum</h1>
+      {course.map((course) => (
+        <div key={course.id}>
+          <Header course={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
         </div>
-    ) 
-}
-export default Course
+      ))}
+    </div>
+  );
+};
+export default Course;
