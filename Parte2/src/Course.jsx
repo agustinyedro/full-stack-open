88e9f714491ace6fa8240abfbbdf7efc77dbccd8
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2024-04-07 23:15:34 -0300

    exercise 2.4

--- Parte2/src/Course.jsx
+++ Parte2/src/Course.jsx
@@ -5,10 +5,13 @@ import Total from "./Total"
 const Course = ({course}) => {
     return (
         <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-          </div>
+            <Header course={course[0].name} />
+            <Content parts={course[0].parts} />
+            <Total parts={course[0].parts} />
+            <Header course={course[1].name} />
+            <Content parts={course[1].parts} />
+            <Total parts={course[1].parts} />
+        </div>
     ) 
 }
 export default Course
