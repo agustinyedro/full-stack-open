b1771fcfc6ca95f2a863da9eb1d9bbcf2d4e5bf5
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2024-04-10 23:11:50 -0300

    exercise 2-7 step2

--- Parte2/2-2/src/App.jsx
+++ Parte2/2-2/src/App.jsx
@@ -1,44 +1,59 @@
-import { useState } from 'react'
-import Persons from './components/Persons'
-
+import { useState } from "react";
+import Persons from "./components/Persons";
 
 function App() {
   const [persons, setPersons] = useState([
     {
-      name: 'Arto Hellas',
-    }
-  ])
-  const [newName, setNewName] = useState('')
+      name: "Arto Hellas",
+    },
+  ]);
+  const [newName, setNewName] = useState("");
 
   const addPerson = (event) => {
-    event.preventDefault()
-    const personObject = {
-      name: newName,
+    event.preventDefault();
+
+    if (existPerson(newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    } else if (newName === "") {
+      alert("Name cannot be empty");
+      return;
+    } else {
+      const personObject = {
+        name: newName,
+      };
+      setPersons(persons.concat(personObject));
+      setNewName("");
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-  }
+  };
   const handleNameChange = (event) => {
-      setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
+
+  const existPerson = (person) => {
+    return persons.find((p) => p.name === person)
+  
+  };
 
   return (
-  <div>
-    <h2>Phonebook</h2>
-    <form onSubmit={addPerson}>
-      <div>
-        name : <input value={newName} onChange={handleNameChange}/>
-      </div>
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
-    <h2>Numbers</h2>
-    <ul>
-      {persons.map(person => <Persons person={person.name}/>)}
-    </ul>
-  </div> 
-  )
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name : <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <Persons person={person.name} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
